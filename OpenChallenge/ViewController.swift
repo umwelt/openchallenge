302519b01030021e9d73c391f21145becc5a79a3
Hugo Perez <supertramp@hugos-mbp.home>
2023-04-24 22:44:29 +0100

    feat: initial commit

---
+++ OpenChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenChallenge
+//
+//  Created by Hugo Perez on 24/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
